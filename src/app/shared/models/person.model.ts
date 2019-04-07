47aeb83a555d2d91cdbdc6b4cef9b40976f36b4d
GEORGE PILITSOGLOU <george.pilitsoglou@gmail.com>
2019-04-07 20:45:23 +0100

    tide: added constructor to person model

--- src/app/shared/models/person.model.ts
+++ src/app/shared/models/person.model.ts
@@ -1,6 +1,18 @@
 export class Person {
   name: string;
-  yearOfBirth: number;
-  monthOfBirth: number;
   dayOfBirth: number;
+  monthOfBirth: number;
+  yearOfBirth: number;
+
+  constructor(
+    name: string,
+    dayOfBirth: number,
+    monthOfBirth: number,
+    yearOfBirth: number
+    ) {
+    this.name = name;
+    this.dayOfBirth = dayOfBirth;
+    this.monthOfBirth = monthOfBirth;
+    this.yearOfBirth = yearOfBirth;
+  }
 }
