6bd3dc678facbd31abf3b152996b047acede37be
GEORGE PILITSOGLOU <george.pilitsoglou@gmail.com>
2019-04-07 18:55:00 +0100

    tide: adding material design button module and moving router-outlet to header

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -3,16 +3,19 @@ import { NgModule } from '@angular/core';
 import {
   MatIconModule,
   MatSidenavModule,
-  MatToolbarModule
+  MatToolbarModule,
+  MatButtonModule
 } from '@angular/material';
 
 @NgModule({
   imports: [
+    MatButtonModule,
     MatIconModule,
     MatSidenavModule,
     MatToolbarModule
   ],
   exports: [
+    MatButtonModule,
     MatIconModule,
     MatSidenavModule,
     MatToolbarModule],
