fa499922a91bebf04fc510a1339f791ca6aa47f8
GEORGE PILITSOGLOU <george.pilitsoglou@gmail.com>
2019-04-07 18:37:38 +0100

    tide: aded flex layout third party

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { MaterialModule } from './material.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
+    FlexLayoutModule,
     HttpClientModule,
     ReactiveFormsModule,
     RouterModule
